0a3acfb1621553117112e5f3e55d522581a385c0
YQLinsanity <yuqiang.lin@stonybrook.edu>
2021-11-17 19:00:01 -0500

    added raycast

--- Source/WolfiesPancakeParty/WolfiesPancakePartyGameMode.cpp
+++ Source/WolfiesPancakeParty/WolfiesPancakePartyGameMode.cpp
@@ -9,7 +9,7 @@ AWolfiesPancakePartyGameMode::AWolfiesPancakePartyGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Blueprints/BP_WolfiesPancakePartyCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
